56e05fb951aea040bdab120ce42f2e9662b3b47a
Murat <63264874+bayirgulu@users.noreply.github.com>
2021-11-25 22:40:44 +0300

    Add files via upload

---
+++ ServisTest/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace ServisTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGirisYap = new System.Windows.Forms.Button();
+            this.btnBakiyeSorgula = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnFaturaGoster = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnGirisYap
+            // 
+            this.btnGirisYap.Location = new System.Drawing.Point(203, 12);
+            this.btnGirisYap.Name = "btnGirisYap";
+            this.btnGirisYap.Size = new System.Drawing.Size(95, 63);
+            this.btnGirisYap.TabIndex = 0;
+            this.btnGirisYap.Text = "Giriş Yap";
+            this.btnGirisYap.UseVisualStyleBackColor = true;
+            this.btnGirisYap.Click += new System.EventHandler(this.btnGirisYap_Click);
+            // 
+            // btnBakiyeSorgula
+            // 
+            this.btnBakiyeSorgula.Location = new System.Drawing.Point(342, 12);
+            this.btnBakiyeSorgula.Name = "btnBakiyeSorgula";
+            this.btnBakiyeSorgula.Size = new System.Drawing.Size(110, 63);
+            this.btnBakiyeSorgula.TabIndex = 1;
+            this.btnBakiyeSorgula.Text = "Bakiye Sorgula";
+            this.btnBakiyeSorgula.UseVisualStyleBackColor = true;
+            this.btnBakiyeSorgula.Click += new System.EventHandler(this.btnBakiyeSorgula_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(69, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(69, 53);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(924, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 17);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "label1";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 195);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(1135, 283);
+            this.dataGridView1.TabIndex = 5;
+            this.dataGridView1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.dataGridView1_MouseDoubleClick);
+            // 
+            // btnFaturaGoster
+            // 
+            this.btnFaturaGoster.Location = new System.Drawing.Point(517, 12);
+            this.btnFaturaGoster.Name = "btnFaturaGoster";
+            this.btnFaturaGoster.Size = new System.Drawing.Size(107, 69);
+            this.btnFaturaGoster.TabIndex = 6;
+            this.btnFaturaGoster.Text = "Fatura göster";
+            this.btnFaturaGoster.UseVisualStyleBackColor = true;
+            this.btnFaturaGoster.Click += new System.EventHandler(this.btnFaturaGoster_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1170, 490);
+            this.Controls.Add(this.btnFaturaGoster);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnBakiyeSorgula);
+            this.Controls.Add(this.btnGirisYap);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGirisYap;
+        private System.Windows.Forms.Button btnBakiyeSorgula;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnFaturaGoster;
+    }
+}
+
---
+++ ServisTest/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+using System;
+
+using System.Windows.Forms;
+using ServisTest.ServiceReference1;
+using System.IO;
+using System.Diagnostics;
+
+namespace ServisTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        BasicHttpBinding binding = new BasicHttpBinding();
+
+     
+
+        private ServiceReference1.EFaturaEntegrasyon2SoapClient client;
+
+        private string token;
+        public long efaturaCredit = 0;
+        public long earsivCredit = 0;
+        public long eirsaliyeCredit = 0;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBox1.Text = "efatura1";
+            textBox2.Text = "Efatura123!";
+
+            binding.Name = "IntegrationServiceSoap";
+            binding.CloseTimeout = TimeSpan.Parse("00:01:00");
+            binding.OpenTimeout = TimeSpan.Parse("00:01:00");
+            binding.ReceiveTimeout = TimeSpan.Parse("00:01:00");
+            binding.SendTimeout = TimeSpan.Parse("00:01:00");
+            binding.AllowCookies = false;
+            binding.BypassProxyOnLocal = false;
+            binding.HostNameComparisonMode = HostNameComparisonMode.StrongWildcard;
+            binding.MaxBufferSize = int.MaxValue; // 65536;
+            binding.MaxBufferPoolSize = int.MaxValue;
+            binding.MaxReceivedMessageSize = int.MaxValue; // 65536;
+            binding.MessageEncoding = WSMessageEncoding.Text;
+            binding.TextEncoding = Encoding.UTF8;
+            binding.TransferMode = TransferMode.Buffered;
+            binding.UseDefaultWebProxy = true;
+            binding.ReaderQuotas.MaxArrayLength = int.MaxValue; // 16384;
+            binding.ReaderQuotas.MaxBytesPerRead = 4096;
+            binding.ReaderQuotas.MaxNameTableCharCount = int.MaxValue; // 16384;
+
+            client = new EFaturaEntegrasyon2SoapClient(binding, new EndpointAddress("https://efintws.turkkep.com.tr/EFaturaEntegrasyon2.asmx?wsdl"));
+        }
+        
+
+        private void btnGirisYap_Click(object sender, EventArgs e)
+        {
+            token = client.OturumAc(textBox1.Text,textBox2.Text);
+            MessageBox.Show("Oturum Açıldı");
+
+        }
+
+        private void btnBakiyeSorgula_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(token))
+            {
+                efaturaCredit = client.EFaturaKalanKontorSorgula(token);
+                earsivCredit = client.EArsivKalanKontorSorgula(token);
+                eirsaliyeCredit = client.EIrsaliyeKalanKontorSorgula(token);
+
+                label1.Text = "E-Fatura Bakiye: " + efaturaCredit +
+                "\nE-Arşiv Bakiye: " + earsivCredit +
+                "\nE-İrsaliye Bakiye: " + eirsaliyeCredit;
+            }
+        }
+
+        private void btnFaturaGoster_Click(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = client.YeniGelenFaturalariListele(token);
+        }
+
+        private void dataGridView1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            var invoice = dataGridView1.CurrentRow.Cells[3].Value.ToString();
+
+            var bytes = client.GelenFaturaPdfAl(token, Convert.ToInt32(invoice));
+            if (bytes != null && bytes.Length > 0)
+            {
+                File.WriteAllBytes(@"C:\TEMP\" + invoice + ".pdf", bytes);
+                Process.Start(@"C:\TEMP\" + invoice + ".pdf");
+            }
+            else
+            {
+                MessageBox.Show("Fatura bulunamadı");
+            }
+        }
+    }
+}
